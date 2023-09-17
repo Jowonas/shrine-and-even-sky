761207544db046ab85ca21a22850b15e32df33c7
EpicCheeseCat <unconfigured@null.spigotmc.org>
2023-09-18 02:53:37 +0800

    did all the stuff

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
 def main():
-    print("Hewwo Shrine and Even Sky peole")
+    import random
+    seggs = (random.randint(1, 100))
+    guess = int(input("Gib me ur guess 1-100"))
+    if seggs==guess:
+        print("damn right")
+    elif guess < seggs:
+        print("ur lower")
+    elif guess > seggs:
+        print("ur higher")
+
+
+
+
+
+
+    print(seggs)
     #fsafafesf
+
 
 
 if __name__ == "__main__":
