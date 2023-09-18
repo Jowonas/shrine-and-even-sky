5886a63fe23f5f5506bb6f6cb5dc79235165f28f
EpicCheeseCat <unconfigured@null.spigotmc.org>
2023-09-19 00:52:15 +0800

    did all the stuff for real this time

--- src/main.py
+++ src/main.py
@@ -2,19 +2,19 @@ def main():
     import random
     seggs = (random.randint(1, 100))
     guess = int(input("Gib me ur guess 1-100"))
-    if seggs==guess:
-        print("damn right")
-    elif guess < seggs:
-        print("ur lower")
-    elif guess > seggs:
-        print("ur higher")
 
+    while guess != seggs:
 
+        if guess < seggs:
 
+            print("ur lower")
+        elif guess > seggs:
 
+            print("ur higher")
+        guess = int(input("Gib me ur guess 1-100: "))
 
+    print("damn right")
 
-    print(seggs)
     #fsafafesf
 
 
